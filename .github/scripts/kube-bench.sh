7fe5082e3a837653b5deba4b8975be9130da1150
Fred Nguyen <nguyentrungtinc4nh1516@gmail.com>
2023-08-05 14:40:58 +0700

    ci(cis-benchmark): use inline Kube-bench YAML manifests

--- .github/scripts/kube-bench.sh
+++ .github/scripts/kube-bench.sh
@@ -1,8 +1,49 @@
 #!/bin/bash 
 # kube-bench.sh
 
-echo "|x| Performing Kube-bench Scanning..."
-kubectl apply -f https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-aks.yaml 
+echo "|x| Performing AQUA Security Kube-bench Scanning..."
+cat <<EOF | kubectl apply -f -
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: kube-bench
+spec:
+  template:
+    spec:
+      hostPID: true
+      containers:
+        - name: kube-bench
+          image: docker.io/aquasec/kube-bench:latest
+          command:
+            ["kube-bench", "run", "--targets", "master,node,etcd,policies", "--benchmark", "cis-1.20"]
+          volumeMounts:
+            - name: var-lib-kubelet
+              mountPath: /var/lib/kubelet
+              readOnly: true
+            - name: etc-systemd
+              mountPath: /etc/systemd
+              readOnly: true
+            - name: etc-default
+              mountPath: /etc/default
+              readOnly: true
+            - name: etc-kubernetes
+              mountPath: /etc/kubernetes
+              readOnly: true
+      restartPolicy: Never
+      volumes:
+        - name: var-lib-kubelet
+          hostPath:
+            path: "/var/lib/kubelet"
+        - name: etc-systemd
+          hostPath:
+            path: "/etc/systemd"
+        - name: etc-default
+          hostPath:
+            path: "/etc/default"
+        - name: etc-kubernetes
+          hostPath:
+            path: "/etc/kubernetes" 
+EOF
 kubectl wait --for=condition=complete --timeout=180s job/kube-bench
 kubectl logs job/kube-bench
 kubectl delete job/kube-bench
